229934e0c03d297625732a0699d24bdccce2d480
Chedli Redissi <credissi@online.saba.com>
2020-01-22 15:56:06 -0500

    adding the project and assignment

---
+++ basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'warning-alert',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.css']
+})
+
+export class WarningComponent {
+}
