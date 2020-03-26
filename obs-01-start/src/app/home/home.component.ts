e5fb7709576cf8fddc01421bafd12f82897fb6a2
Chedli Redissi <credissi@online.saba.com>
2020-03-26 15:50:25 -0400

    13

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  obs: Observable<number>;
 
   constructor() { }
 
   ngOnInit() {
+    this.obs = Observable.create( observer => {
+      let count = 0;
+      setInterval( () => {
+        if (count > 5) {
+          observer.error(new Error( 'count > 5' ));
+        }
+        observer.next(count++);
+      } , 1000 );
+    });
+    this.obs.pipe( filter( ( data: number ) => {return data % 2 === 0;} ) ).subscribe(
+      data => {console.log(data)},
+      error => console.log(error.message));
+  }
+
+  ngOnDestroy(): void {
+    // this.obs.unsubscribe();
   }
 
 }
